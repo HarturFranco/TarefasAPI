d8a876a7b9e11a4932a3155e60796cd56e36477f
ThiagoSallesSantos <thiagosallessantos3@gmail.com>
2022-08-21 14:15:43 -0300

    Demo

--- tarefaA/settings.py
+++ tarefaA/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATIC_URL = "static/"
+STATIC_URL = BASE_DIR / "staticfiles"
 
 DISABLE_COLLECTSTATIC = 1
 
